ff1190470fd31f814addb9a7a5fac455ede23891
helloily100 <80566455+helloily100@users.noreply.github.com>
2022-11-19 15:50:53 -0800

    Update Return.py

--- 323/Phase 2/Return.py
+++ 323/Phase 2/Return.py
@@ -10,7 +10,7 @@ class Return(Base):
     key_requests_copy_keys_id = Column(Integer, ForeignKey('key_requests.copy_keys_id'), primary_key=True, nullable=False)
     key_requests_copy_keys_is_loss = Column(Integer, ForeignKey('key_requests.copy_keys_is_loss'), primary_key=True, nullable=False)
     
-    def __init__(self, report_date: Date, key_request, key_request, key_request, key_request):
+    def __init__(self, report_date: Date, key_request):
         self.report_date = report_date
         self.key_requests_employees_id = key_request.key_requests_employees_id
         self.key_requests_rooms_num = key_request.key_requests_rooms_num
