472ec7d0e7c116f151b8f62cdb8368e32213dae2
helloily100 <80566455+helloily100@users.noreply.github.com>
2022-11-19 15:07:21 -0800

    Update CopyKey.py

--- 323/Phase 2/CopyKey.py
+++ 323/Phase 2/CopyKey.py
@@ -3,12 +3,17 @@ from sqlalchemy.orm import relationship
 from orm_base import Base
 
 class CopyKey(Base):
-    __tablename__ = "copy_key"
-    keyNumber = Column("key_number", Integer, nullable=False)
+    __tablename__ = "copy_keys"
+    id = Column('id', Integer, Identity(start=1, cycle=True),
+                       nullable=False, primary_key=True)
     issuedDate = Column("issued_date", Date, nullable=False)
     issuedTime = Column("issued_time", Time, nullable=False)
+    hooks_id = Column(Intger, ForeignKey('hooks.hooks_id'), nullable=False)
+    is_loss = ('is_loss', Boolean, primary_key=True, nullable=False)
     
-    def __init__(self, keyNumber: Integer, issuedDate: Date, issuedTime: Time):
-        self.keyNumber = keyNumber
+    def __init__(self, keyNumber: Integer, issuedDate: Date, issuedTime: Time, hook, is_loss: Boolean):
+        self.id = id
         self.issuedDate = issuedDate
         self.issuedTime = issuedTime
+        self.hooks_id = hook.hooks_id
+        self.is_loss = is_loss
