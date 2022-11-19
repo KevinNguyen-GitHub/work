2ea0df0f324b60a6dbe59427e3b2b1c995b2879f
helloily100 <80566455+helloily100@users.noreply.github.com>
2022-11-19 15:24:34 -0800

    Update Loss.py

--- 323/Phase 2/Loss.py
+++ 323/Phase 2/Loss.py
@@ -4,7 +4,15 @@ from orm_base import Base
 
 class Loss(Base):
     __tablename__ = "loss"
-    reportLossDate = Column("report_loss_date", Date, nullable=False)
+    report_date = Column("report_date", Date, primary_key=True, nullable=False)
+    key_requests_employees_id = Column(Integer, ForeignKey('key_requests.employees_id'), primary_key=True, nullable=False)
+    key_requests_rooms_num = Column(Integer, ForeignKey('key_requests.rooms_num'), primary_key=True, nullable=False)
+    key_requests_copy_keys_id = Column(Integer, ForeignKey('key_requests.copy_keys_id'), primary_key=True, nullable=False)
+    key_requests_copy_keys_is_loss = Column(Integer, ForeignKey('key_requests.copy_keys_is_loss'), primary_key=True, nullable=False)
     
-    def __init__(self, reportLossDate: Date):
-        self.reportLossDate = reportLossDate
+    def __init__(self, report_date: Date, key_request, key_request, key_request, key_request):
+        self.report_date = report_date
+        self.key_requests_employees_id = key_request.key_requests_employees_id
+        self.key_requests_rooms_num = key_request.key_requests_rooms_num
+        self.key_requests_copy_keys_id = key_request.key_requests_copy_keys_id
+        self.key_requests_copy_keys_is_loss = key_request.key_requests_copy_keys_is_loss
