bea6ce116aa1a12f7d3d78825f143b8c80ca2082
helloily100 <80566455+helloily100@users.noreply.github.com>
2022-11-19 15:14:50 -0800

    Update KeyStatus.py

--- 323/Phase 2/KeyStatus.py
+++ 323/Phase 2/KeyStatus.py
@@ -2,9 +2,17 @@ from sqlalchemy import Column, String, Integer, ForeignKey
 from sqlalchemy.orm import relationship
 from orm_base import Base
 
-class KeyStatus(Base):
-    __tablename__ = "key_status"
-    isLoss = Column("is_loss", Boolean, nullable=False)
+class HookLine(Base):
+    __tablename__ = "hook_lines"
+    doors_door_type_name = Column(String(50), ForeignKey('doors.door_type_name'), primary_key=True, nullable=False)
+    doors_rooms_num = Column(Intger, ForeignKey('doors.rooms_num'), primary_key=True, nullable=False)
+    doors_rooms_buildings_name = Column(String(50), ForeignKey('doors.rooms_buildings_name'), primary_key=True, nullable=False)
+    hooks_id = Column(Integer, Foreign('hooks.id'), primary_key=True, nullable=False)
     
-    def __init__(self, isLoss: Boolean):
-        self.isLoss = isLoss
+    #relationship 
+    
+    def __init__(self, door, door, door, hook):
+        self.doors_door_type_name = door.doors_door_type_name
+        self.doors_rooms_num = door.doors_rooms_num
+        self.doors_rooms_buildings_name =door.doors_rooms_buildings_name 
+        self.hooks_id = hook.hooks_id
