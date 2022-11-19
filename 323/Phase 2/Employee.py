ffbbc96318eabc429ff2168f8dff97760a41bf00
helloily100 <80566455+helloily100@users.noreply.github.com>
2022-11-19 14:59:52 -0800

    Update Employee.py

--- 323/Phase 2/Employee.py
+++ 323/Phase 2/Employee.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import relationship
 from orm_base import Base
 
 class Employee(Base):
-    __tablename__ = "employee"
+    __tablename__ = "employees"
     name = Column("name", String(40), nullable=False)
     id = Column("id", Integer, Identity(start=1, cycle=True),
                        nullable=False, primary_key=True))
