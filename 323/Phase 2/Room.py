3135b76dda8c0f0ca07ea4069d154d73fd584a8a
helloily100 <80566455+helloily100@users.noreply.github.com>
2022-11-19 14:59:24 -0800

    Update Room.py

--- 323/Phase 2/Room.py
+++ 323/Phase 2/Room.py
@@ -3,9 +3,10 @@ from sqlalchemy.orm import relationship
 from orm_base import Base
 
 class Room(Base):
-    __tablename__ = "room"
-    roomNum = Column("room_num", Integer, nullable=False)
+    __tablename__ = "rooms"
+    num = Column("num", Integer, primary_key=True, nullable=False)
+    buildings_name(String(50), ForiegnKey('buildings.name'), primary_key=True, nullable=False)
     
     
-    def __init__(self, roomNum: Integer):
-        self.roomNum = roomNum
+    def __init__(self, num: Integer):
+        self.num = num
