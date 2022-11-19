b701ec80b61a7bfc1b0f84b35aa9317ce88d3d6f
helloily100 <80566455+helloily100@users.noreply.github.com>
2022-11-19 15:47:22 -0800

    Update Hook.py

--- 323/Phase 2/Hook.py
+++ 323/Phase 2/Hook.py
@@ -1,11 +1,25 @@
 from sqlalchemy import Column, String, Integer, ForeignKey
 from sqlalchemy.orm import relationship
 from orm_base import Base
+from HookLine import HookLine
 
 class Hook(Base):
-    __tablename__ = "hooks"
+    __tablebname__ = "hooks"
     id = Column('id', Integer, Identity(start=1, cycle=True),
-                       nullable=False, primary_key=True)
-    
+                nullable=False, primary_key=True)
+    doors_list: [HookLine] = relationship("Unknown", back_populates="hook", viewonly=False)
     def __init__(self, id: Integer):
         self.id = id
+        self.doors_list = []
+
+    def add_door(self, door):
+        # make sure this genre is non already on the list.
+        for next_door in self.doors_list:
+            if next_door == door:
+                return
+        # Create an instance of the junction table class for this relationship.
+        hook_list = HookLine(door,self)
+        # Update this move to reflect that we have this genre now.
+        door.hooks_list.append(hook_list)
+        # Update the genre to reflect this movie.
+        self.doors_list.append(hook_list)
