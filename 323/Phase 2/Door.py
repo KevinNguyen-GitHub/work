6f7fe0233b954fbc77050fb7210b07c0e545cb58
helloily100 <80566455+helloily100@users.noreply.github.com>
2022-11-19 14:56:07 -0800

    Update Door.py

--- 323/Phase 2/Door.py
+++ 323/Phase 2/Door.py
@@ -3,8 +3,14 @@ from sqlalchemy.orm import relationship
 from orm_base import Base
 
 class Door(Base):
-    __tablename__ = "door"
-    doorType = Column("door_type", String(40), nullable=False)
+    __tablename__ = "doors"
+    door_type_name = Column(String(40), ForeignKey('door_type.name'), primary_key=True, nullable=False)
+    rooms_num = Column(Integer, ForeignKey('rooms.num'), primary_key=True, nullable=False)
+    rooms_buildings_name = Column(String(50), ForeignKey('rooms.buildings_name'), primary_key=True, nullable=False)
     
-    def __ini__(self, doorType: String):
-        self.doorType = doorType
+    #relationship 
+    
+    def __ini__(self, door_type, room, room):
+        self.door_type_name = door_type.door_type_name
+        self.rooms_num = room.rooms_num
+        self.rooms_buildings_name = room.rooms_buildings_name
