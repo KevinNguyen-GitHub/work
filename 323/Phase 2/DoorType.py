f61ea00643c3b23e5610ebbec8777723dd32942b
helloily100 <80566455+helloily100@users.noreply.github.com>
2022-11-19 12:27:54 -0800

    Create DoorType.py

---
+++ 323/Phase 2/DoorType.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, Identity, Float, \
+    String, UniqueConstraint
+from sqlalchemy.orm import relationship
+
+from orm_base import Base
+from HookLine import HookLine
+
+class DoorType(Base):
+    __tablebname__ = "door_type"
+    name = Column('name', String(100), primary_key= True, nullable=False)
+
+    def __init__(self, name: String):
+        self.name = name
+
+
