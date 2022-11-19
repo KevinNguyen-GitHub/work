fff52178ddc1d6a1cca9c4062170cc0285b44bd4
helloily100 <80566455+helloily100@users.noreply.github.com>
2022-11-19 12:29:37 -0800

    Update Employee.py

--- 323/Phase 2/Employee.py
+++ 323/Phase 2/Employee.py
@@ -2,11 +2,11 @@ from sqlalchemy import Column, String, Integer, ForeignKey
 from sqlalchemy.orm import relationship
 from orm_base import Base
 
-class Emplyoee(Base):
+class Employee(Base):
     __tablename__ = "employee"
     name = Column("name", String(40), nullable=False)
-    emplyoeeID = Column("employee_id", Integer, nullable=False)
+    employee_id = Column("employee_id", Integer, Identity(start=1, cycle=True),
+                       nullable=False, primary_key=True))
     
-def _init_(self, name: String, employeeID: Integer):
+def _init_(self, name: String):
     self.name = name
-    self.employeeID = employeeID 
