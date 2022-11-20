d5f406fd7aa9348c87d02e6dfefbf44c7847aba8
KevinNguyen-GitHub <nguyenkevin828@gmail.com>
2022-11-19 17:41:48 -0800

    fixes

--- 323/Phase 2/Building.py
+++ 323/Phase 2/Building.py
@@ -2,10 +2,11 @@ from sqlalchemy import Column, String, Integer, Date, Time, Boolean, Identity, F
 from sqlalchemy.orm import relationship
 from orm_base import Base
 
+
 class Building(Base):
     __tablename__ = "buildings"
     name = Column("name", String, primary_key=True, nullalbe=False)
     room = relationship("Room")
-    
+
     def __init__(self, name: String):
         self.name = name
--- 323/Phase 2/CopyKey.py
+++ 323/Phase 2/CopyKey.py
@@ -2,15 +2,16 @@ from sqlalchemy import Column, String, Integer, Date, Time, Boolean, Identity, F
 from sqlalchemy.orm import relationship
 from orm_base import Base
 
+
 class CopyKey(Base):
     __tablename__ = "copy_keys"
     id = Column('id', Integer, Identity(start=1, cycle=True),
-                       nullable=False, primary_key=True)
+                nullable=False, primary_key=True)
     issuedDate = Column("issued_date", Date, nullable=False)
     issuedTime = Column("issued_time", Time, nullable=False)
-    hooks_id = Column(Intger, ForeignKey('hooks.hooks_id'), nullable=False)
-    is_loss = ('is_loss', Boolean, primary_key=True, nullable=False)
-    
+    hooks_id = Column(Integer, ForeignKey('hooks.hooks_id'), nullable=False)
+    is_loss = Column("is_loss", Boolean, nullable=False, primary_key=True)
+
     def __init__(self, keyNumber: Integer, issuedDate: Date, issuedTime: Time, hook, is_loss: Boolean):
         self.id = id
         self.issuedDate = issuedDate
