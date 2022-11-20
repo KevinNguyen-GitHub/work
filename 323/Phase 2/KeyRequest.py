144bb671f56c12d221709e17025abe588803de58
helloily100 <80566455+helloily100@users.noreply.github.com>
2022-11-19 17:12:24 -0800

    Update KeyRequest.py

--- 323/Phase 2/KeyRequest.py
+++ 323/Phase 2/KeyRequest.py
@@ -5,15 +5,17 @@ from orm_base import Base
 
 class KeyRequest(Base):
     __tablename__ = "key_requests"
+    request_id = Column('request_id', Integer, Identity(start=1, cycle=True),
+                       nullable=False, primary_key=True) 
     request_date = Column("request_date", Date, nullable=False)
     #foreign key from employee class
-    employee_id = Column(Integer, ForeignKey('employees.id'), primary_key=True, nullable=False)
+    employee_id = Column(Integer, ForeignKey('employees.id'), nullable=False)
     #foreign key from room class
-    rooms_num = Column(Integer, ForeignKey('rooms.num'), primary_key=True, nullable=False)
-    rooms_buildings_name = Column(Integer, ForeignKey('rooms.buildings_name'), primary_key=True, nullable=False)
+    rooms_num = Column(Integer, ForeignKey('rooms.num'), nullable=False)
+    rooms_buildings_name = Column(Integer, ForeignKey('rooms.buildings_name'), nullable=False)
     #foreign key from copy_keys class 
-    copy_keys_id = Column(Integer, ForeignKey('copy_keys.id'), primary_key=True, nullable=False)
-    copy_keys_is_loss = Column(Boolean, ForeignKey('copy_keys.is_loss'), primary_key=True, nullable=False)
+    copy_keys_id = Column(Integer, ForeignKey('copy_keys.id'), nullable=False)
+    copy_keys_is_loss = Column(Boolean, ForeignKey('copy_keys.is_loss'), nullable=False)
     
     #relationship
     employee = relationship("Employee", back_populates= 'rooms_list')
