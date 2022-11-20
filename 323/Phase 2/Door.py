9b75305774d953d8fc61eb2c64490f2de300ae35
carinegordillo <64708532+carinegordillo@users.noreply.github.com>
2022-11-19 19:44:22 -0800

    cg

--- 323/Phase 2/Door.py
+++ 323/Phase 2/Door.py
@@ -2,18 +2,20 @@ from sqlalchemy import Column, String, Integer, ForeignKey
 from sqlalchemy.orm import relationship
 from orm_base import Base
 
+from Room import Room
+from DoorType import DoorType
 from HookLine import HookLine
-
-
 class Door(Base):
     __tablename__ = "doors"
-    door_type_name = Column(String(40), ForeignKey('door_type.name'), primary_key=True, nullable=False)
-    rooms_num = Column(Integer, ForeignKey('rooms.num'), primary_key=True, nullable=False)
+    door_type_name = Column('door_type_name',String(40), ForeignKey('door_type.name'), primary_key=True, nullable=False)
+    rooms_num = Column('room_num',Integer, ForeignKey('rooms.number'), primary_key=True, nullable=False)
     rooms_buildings_name = Column(String(50), ForeignKey('rooms.buildings_name'), primary_key=True, nullable=False)
-
-    # relationship
-    hooks_list: [HookLine] = relationship("HookLine", back_populates="door", viewonly=False)
-
+    
+    #relationship
+    doortype= relationship("DoorType", back_populates="doors", viewonly=False)
+    hooks_list: [HookLine] = relationship("HookLine", back_populates="doors", viewonly=False)
+    rooms = relationship("Room", back_populates="doors", viewonly=False)
+    
     def __init__(self, door_type, room):
         self.door_type_name = door_type.door_type_name
         self.rooms_num = room.rooms_num
@@ -23,10 +25,11 @@ class Door(Base):
     def add_hook(self, hook):
         for next_hook in self.hooks_list:
             if next_hook == hook:
+                print("Hook has already been created at a previous time")
                 return
         # Create an instance of the junction table class.
-        hook_list = (hook, self)
+        hook_line = HookLine(hook, self)
         # add that new instance to the list of genres that the Movie keeps.
-        hook.doors_list.append(hook_list)
+        hook.add_door(hook_line)
         # add that new instance to the list of movies that this genre keeps.
-        self.hooks_list.append(hook_list)
+        self.hooks_list.append(hook_line)
