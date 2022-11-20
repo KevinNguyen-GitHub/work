7c55896bf2be2dd8204e7c162b1b832d20677f30
carinegordillo <64708532+carinegordillo@users.noreply.github.com>
2022-11-19 17:47:31 -0800

    Update Room.py
    room change

--- 323/Phase 2/Room.py
+++ 323/Phase 2/Room.py
@@ -1,26 +1,20 @@
-from sqlalchemy import Column, String, Integer, Date, Time, Boolean, Identity, ForeignKey
+from sqlalchemy import Column, String, Integer, ForeignKey
 from sqlalchemy.orm import relationship
 from orm_base import Base
 
-from KeyRequest import KeyRequest
+from Building import Building
+
 class Room(Base):
     __tablename__ = "rooms"
-    num = Column("num", Integer, primary_key=True, nullable=False)
+    number = Column("number", Integer, primary_key=True, nullable=False)
     buildings_name = Column(String(50), ForeignKey('buildings.name'), primary_key=True, nullable=False)
     
-    door = relationship("Door")
-    employees_list: [KeyRequest] = relationship("KeyRequest", back_populates="room", viewonly=False) 
-    def __init__(self, num: Integer, building):
-        self.num = num
+    door = relationship("Door", back_populates="rooms",  viewonly=False)
+    requests = relationship("Requests", back_populates="rooms", viewonly=False)
+    buildings = relationship("Buildings", back_populates="rooms", viewonly=False)
+
+
+    
+    def __init__(self, number: Integer, building):
+        self.number = number
         self.buildings_name = building.buildings_name
-        
-    def add_employee(self, employee):
-        for next_employee in self.employees_list:
-            if next_employee == employee:
-                return
-        # Create an instance of the junction table class.
-        key_request = KeyRequest(employee, self)
-        # add that new instance to the list of genres that the Movie keeps.
-        employee.rooms_list.append(key_request)
-        # add that new instance to the list of movies that this genre keeps.
-        self.employees_list.append(key_request)
