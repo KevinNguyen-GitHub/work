664c04cfb872a0f6de531f228a42fe500e760e2b
carinegordillo <64708532+carinegordillo@users.noreply.github.com>
2022-11-19 19:44:59 -0800

    cg

--- 323/Phase 2/Hook.py
+++ 323/Phase 2/Hook.py
@@ -1,16 +1,17 @@
-from sqlalchemy import Column, String, Integer, Date, Time, Boolean, Identity, ForeignKey
+from sqlalchemy import Column, String, Integer, ForeignKey, Identity
 from sqlalchemy.orm import relationship
 from orm_base import Base
+
 from HookLine import HookLine
 
-
 class Hook(Base):
-    __tablename__ = "hooks"
-    id = Column('id', Integer, Identity(start=1, cycle=True),
+    __tablebname__ = "hooks"
+    id = Column('id', Integer, Identity('hook_num',start=1, cycle=True),
                 nullable=False, primary_key=True)
-    copy_key = relationship("CopyKey")
-    doors_list: [HookLine] = relationship("HookList", back_populates="hook", viewonly=False)
 
+    copy_key = relationship("CopyKey", back_populates="hook")
+
+    doors_list: [HookLine] = relationship("HookLine", back_populates="hook", viewonly=False)
     def __init__(self, id: Integer):
         self.id = id
         self.doors_list = []
@@ -21,8 +22,9 @@ class Hook(Base):
             if next_door == door:
                 return
         # Create an instance of the junction table class for this relationship.
-        hook_list = HookLine(self, door)
+        hook_list = HookLine(self,door)
         # Update this move to reflect that we have this genre now.
         door.hooks_list.append(hook_list)
         # Update the genre to reflect this movie.
         self.doors_list.append(hook_list)
+
