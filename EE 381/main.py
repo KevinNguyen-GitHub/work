d1b608f298af32c15bddb5ed7caddd9f2b0af2b2
KevinNguyen-GitHub <nguyenkevin828@gmail.com>
2022-12-16 14:17:05 -0800

    new

--- EE 381/main.py
+++ EE 381/main.py
@@ -1,118 +1,84 @@
-# import matplotlib.pyplot as plt
-# import numpy as np
-
-# # Read in the data from the CSV file
-# csv_file = 'Sales_01_20.csv'
-# data = np.loadtxt(csv_file, delimiter = ',', skiprows = 1)
-        
-# # calculate the mean
-# def mean(numbers):
-#     """Calculate the mean of an array of numbers"""
-#     return sum(numbers) / len(numbers)
-
-# # calculate the std
-# def std(numbers):
-#     """Calculate the standard deviation of an array of numbers"""
-#     mean = mean(numbers)
-#     variance = sum((x - mean) ** 2 for x in numbers) / len(numbers)
-#     return variance ** 0.5
-
-# # Calculate the mean and standard deviation for each year
-# yearly_mean = []
-# yearly_std = []
-# for year in range(2001, 2021):
-#     year_data = [x for x in data if x >= year and x < year+1]
-#     mean = mean(year_data)
-#     std = std(year_data)
-#     yearly_mean.append(mean)
-#     yearly_std.append(std)
-
-# # Calculate the probability of sale prices within a certain range
-# lower_bound = 200000
-# upper_bound = 300000
-# yearly_probability = []
-# for year in range(2001, 2021):
-#     year_data = [x for x in data if x >= year and x < year+1]
-#     num_within_range = len([x for x in year_data if lower_bound <= x <= upper_bound])
-#     probability = num_within_range / len(year_data)
-#     yearly_probability.append(probability)
-
-# # Plot the mean values
-# plt.hist(data, ec = 'blue')
-# plt.title('Yearly Mean Sale Prices')
-# plt.xlabel('Year')
-# plt.ylabel('Mean Sale Price')
-# plt.show()
-
-# # Plot the standard deviation values
-# plt.hist(data, ec = 'blue')
-# plt.title('Yearly Standard Deviation of Sale Prices')
-# plt.xlabel('Year')
-# plt.ylabel('Standard Deviation')
-# plt.show()
-
-# # Plot the probability values
-# plt.hist(data, ec = 'blue')
-# plt.title('Yearly Probability of Sale Prices Within a Certain Range')
-# plt.xlabel('Year')
-# plt.ylabel('Probability')
-# plt.show()
-
-
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from math import sqrt
 
-# Read in the data from the CSV file
-csv_file = 'Sales_01_20.csv'
-data = np.loadtxt(csv_file, delimiter = ',', skiprows = 1)
+# Load the dataset from the CSV file
+df = pd.read_csv('Sales_01_20.csv')
 
-def quicksort(arr):
-    if len(arr) <= 1:
-        return arr
-    pivot = arr[len(arr) // 2]
-    left = [x for x in arr if x < pivot]
-    middle = [x for x in arr if x == pivot]
-    right = [x for x in arr if x > pivot]
-    return quicksort(left) + middle + quicksort(right)
 
-quickSort(data)
+# Calculate mean
+def get_mean(array):
+    return sum(array) / len(array)
 
-# Separate the data into 20 years of data
-years = 20
-data_by_year = np.array_split(data, years)
+# Calculate Std
+def get_std(array):
+    mean_value = sum(array) / len(array)
+    variance = sum((x - mean_value) ** 2 for x in array) / (len(array) - 1)
+    stdev = sqrt(variance)
+    return stdev
 
 # Calculate the mean and standard deviation of sale prices for each year
+years = np.unique(df['Year'])
 means = []
 stds = []
+for year in years:
+    sale_prices_year = df[df['Year'] == year]['Sale Amount']
+    mean = get_mean(sale_prices_year)
+    std = get_std(sale_prices_year)
+    means.append(mean)
+    stds.append(std)
+
+# Show the mean plot
+print('2001:', means[0])
+print('2005:', means[4])
+print('2010:', means[9])
+print('2015:', means[14])
+print('2020:', means[19], '\n')
+
+plt.bar(years, means)
+plt.title('Yearly Mean Sale Prices')
+plt.xlabel('Year')
+plt.ylabel('Mean')
+plt.xticks(np.arange(2000, 2021, 5))
+# Show the plot
+plt.show()
+
+# Show the standard deviation plot
+print('2001:', stds[0])
+print('2005:', stds[4])
+print('2010:', stds[9])
+print('2015:', stds[14])
+print('2020:', stds[19], '\n')
+
+plt.bar(years, stds)
+plt.title('Yearly Standard Deviation of Sale Prices')
+plt.xlabel('Year')
+plt.ylabel('STD')
+plt.xticks(np.arange(2000, 2021, 5))
+# Show the plot
+plt.show()
+
+# Calculate the yearly probability of sale price ranging from $200,000 to $300,000
 probabilities = []
-for year in data_by_year:
-    means.append(np.mean(year))
-    stds.append(np.std(year))
+for year in years:
+    sale_prices_year = df[df['Year'] == year]['Sale Amount']
+    num_in_range = np.sum((sale_prices_year >= 200000) & (sale_prices_year <= 300000))
+    total = len(sale_prices_year)
+    probability = num_in_range / total
+    probabilities.append(probability)
 
-# Calculate the probability of sale prices within a certain range
-lower_bound = 200000
-upper_bound = 300000
-probability = np.mean(np.logical_and(year >= lower_bound, year <= upper_bound))
-probabilities.append(probability)
+# Create a plot for probabilities
+print('2001:', probabilities[0])
+print('2005:', probabilities[4])
+print('2010:', probabilities[9])
+print('2015:', probabilities[14])
+print('2020:', probabilities[19], '\n')
 
-# Plot the mean values as a bar graph
-fig1, ax1 = plt.subplots()
-ax1.bar(range(1, years + 1), means)
-ax1.set_title('Yearly Mean Sale Prices')
-ax1.set_xlabel('Years')
-ax1.set_ylabel('Mean Price')
-
-# Plot the standard deviation values as a bar graph
-fig2, ax2 = plt.subplots()
-ax2.bar(range(1, years + 1), stds)
-ax2.set_title('Yearly Standard Deviation of Sale Prices')
-ax2.set_xlabel('Years')
-ax2.set_ylabel('Standard Deviation')
-
-# Plot the probability values as a bar graph
-fig3, ax3 = plt.subplots()
-ax3.bar(range(1, years + 1), probabilities)
-ax3.set_title('Yearly Probability of Sale Prices Within a Certain Range')
-ax3.set_xlabel('Years')
-ax3.set_ylabel('Probability')
+plt.bar(years, probabilities)
+plt.title('Yearly Probability of Sale Price Being in Range $200,000-$300,000')
+plt.xlabel('Year')
+plt.ylabel('Probabilities')
+plt.xticks(np.arange(2000, 2021, 5))
+# Show the plot
 plt.show()
